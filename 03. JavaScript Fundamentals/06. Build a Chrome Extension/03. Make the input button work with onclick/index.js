06665339a3b15378546b745003c17bac151e28d9
Rahi6023 <rahidulmowlarahi6023@gmail.com>
2026-02-09 22:07:17 +0600

    solved 3.6.5 and 3.6.3

--- 03. JavaScript Fundamentals/06. Build a Chrome Extension/03. Make the input button work with onclick/index.js
+++ 03. JavaScript Fundamentals/06. Build a Chrome Extension/03. Make the input button work with onclick/index.js
@@ -1,1 +1,9 @@
 // Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+
+const btn = document.getElementById("input-btn");
+
+btn.addEventListener("click",()=>{
+    const p = document.createElement('p');
+    p.innerText = "Hi there!";
+    document.body.appendChild(p);
+})
--- 03. JavaScript Fundamentals/06. Build a Chrome Extension/05. Write your first addEvent Listener()/index.js
+++ 03. JavaScript Fundamentals/06. Build a Chrome Extension/05. Write your first addEvent Listener()/index.js
@@ -2,3 +2,9 @@
 // 2. Add a click event listener to the box 
 // 3. Log out "I want to open the box!" when it's clicked
 
+const box = document.getElementById("box");
+box.addEventListener("click",()=>{
+    let p = document.createElement('p');
+    p.innerText = "I want to open the box!";
+    document.body.appendChild(p);
+})
