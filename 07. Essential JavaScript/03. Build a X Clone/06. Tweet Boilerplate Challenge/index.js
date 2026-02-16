f1e811a9009a6d7046ca577e650acacbef48517d
Rahi6023 <rahidulmowlarahi6023@gmail.com>
2026-02-16 20:34:52 +0600

    Started 07.03 and completed 07.03.02-07.03.06

--- 07. Essential JavaScript/03. Build a X Clone/06. Tweet Boilerplate Challenge/index.js
+++ 07. Essential JavaScript/03. Build a X Clone/06. Tweet Boilerplate Challenge/index.js
@@ -16,9 +16,35 @@ Challenge:
 2. Store this HTML in a let called "feedHtml".
 3. Log out feedHtml.
 4. Call getFeedHtml to check it's working.
-*/  
+*/
+    for(let tweets of tweetsData){
+        let feedHtml = `
+    <div class="tweet">
+        <div class="tweet-inner">
+            <img src="${tweets.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${tweets.handle}</p>
+                <p class="tweet-text">${tweets.tweetText}</p>
+                <div class="tweet-details">
+                    <span class="tweet-detail">
+                        ${tweets.replies.length}
+                    </span>
+                    <span class="tweet-detail">
+                        ${tweets.likes}
+                    </span>
+                    <span class="tweet-detail">
+                        ${tweets.retweets}
+                    </span>
+                </div>   
+            </div>            
+        </div>
+    </div>
+        `
+    console.log(feedHtml)
+    }
 }
 
+getFeedHtml()
 
 /*
 <div class="tweet">
